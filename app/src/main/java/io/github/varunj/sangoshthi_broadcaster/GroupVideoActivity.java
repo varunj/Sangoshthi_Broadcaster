546946a0fc069ade6156aa6439e62c79ffce76aa
Varun Jain <Varun Jain>
2017-03-12 23:05:43 +0530

    Minor Fixes

--- app/src/main/java/io/github/varunj/sangoshthi_broadcaster/GroupVideoActivity.java
+++ app/src/main/java/io/github/varunj/sangoshthi_broadcaster/GroupVideoActivity.java
@@ -54,7 +54,7 @@ import java.util.concurrent.LinkedBlockingDeque;
  * Created by Varun on 04-03-2017.
  */
 
-public class GroupVideo extends AppCompatActivity {
+public class GroupVideoActivity extends AppCompatActivity {
     private String receiverGroupName , senderPhoneNum;
     private Message message1;
     Thread publishThread;
@@ -355,7 +355,7 @@ public class GroupVideo extends AppCompatActivity {
         DataSource dataSource = new DefaultUriDataSource(this, null, userAgent);
         ExtractorSampleSource sampleSource = new ExtractorSampleSource(
                 Uri.fromFile(
-                        new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), VIDEO_URI)
+                        new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "/Sangoshthi/" + VIDEO_URI)
                 ),
                 dataSource, allocator, BUFFER_SEGMENT_COUNT * BUFFER_SEGMENT_SIZE);
 
